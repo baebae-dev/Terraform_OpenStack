3b6aaeedee0032a593eb922983d7a250861ae293
bell.coco <bell.coco@kakaoenterprise.com>
2023-03-16 16:15:44 +0900

    fix

--- primitives/backend/backend.tf
+++ primitives/backend/backend.tf
@@ -9,7 +9,7 @@ data "terraform_remote_state" "remote" {
 # remote state에 기록된 생성된 object storage에 object 업로드
 resource "openstack_objectstorage_object_v1" "tfstate_test" {
   region         = "RegionOne"
-  container_name = data.terraform_remote_state.remote.outputs.objectStorage_name
+  container_name = data.terraform_remote_state.remote.outputs.objectStorage
   name           = "env.sh"
   source       = "./env.sh"
 
