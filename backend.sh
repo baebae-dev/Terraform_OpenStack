548486d9207cc8e89b9dc0fb2fb88743f463ded2
bell.coco <bell.coco@kakaoenterprise.com>
2023-03-21 19:04:01 +0900

    backend 수정

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./env.sh
 
-export get_X_Auth_Token="True"
+export get_X_Auth_Token="False"
 # get X_Auth_Token
 if [[ ${get_X_Auth_Token} == "True" ]]; then
   curl -i --location 'https://gov-cbt-keystone.kakaoicloud.in/v3/auth/tokens' \
@@ -36,13 +36,12 @@ if [[ ${get_X_Auth_Token} == "True" ]]; then
   rm result_Object_storage_header.txt
 else
   echo ""
-#  export X_Auth_Token="gAAAAABkGWXyXHl1IsG8rNTyk4HhOG0099AUUOAbQyAy_5_HCNW4ldJzIB3Qbp-LHDopNFNgOYiWpupTg2ONaSdmRWKasF_fTNr0W7mBsTj0gsUpsDu5Er1gtiH39onnOGlbwXi5gATwhq46TPfDd4EpyeV1qypGifXNnkMQBGnyioUeSDqAn0o"
+    export X_Auth_Token="gAAAAABkGX-E9iH-GRhgYfOObGHByQpVl-SUimUux9yn70heHLlRyHzZy0nXGAvKgn0AhKBVTe6rqRqSfDkCZIUYBtVsK8rvW_CG9q-bmVt2M5GVkQKtO-yhHjlv5qwp0YqOQWocjS86LlIAlXIY3Q_6Ay2nOtHmt7gvL6Rfv57e7GJ0b8ca5fY"
 fi
 
 export remote_state_filenm="terraform-test.tfstate"
 export object_storage_url="https://gov-cbt-objectstorage.kakaoicloud.in/v1/8323ba34b836415a98bc45c8b715dcfc/remote-state/${remote_state_filenm}"
 
 # backend 파일 전송
-echo "curl -i ${object_storage_url} -X PUT -d "@terraform.tfstate" -H \"X-Auth-Token: ${X_Auth_Token}\""
-sleep 5
-curl -i ${object_storage_url} -X PUT -d "@terraform.tfstate" -H "X-Auth-Token: ${X_Auth_Token}"
+echo "curl -i ${object_storage_url} -X  PUT -d "@terraform.tfstate" -H \"X-Auth-Token: ${X_Auth_Token}\""
+curl -i ${object_storage_url} -X PUT -d "@terraform.tfstate" -H "X-Auth-Token: ${X_Auth_Token}" --http1.1
