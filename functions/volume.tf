00894a058ea93970fdf57c00a4a91bb3dc826942
bell.coco <bell.coco@kakaoenterprise.com>
2023-03-27 13:29:28 +0900

    volume attach

--- functions/volume.tf
+++ functions/volume.tf
@@ -1,17 +1,11 @@
 # volume
 module "volume" {
-  source        = "../primitives/volume"
+  source       = "../../primitives/volume"
 
-  volume_name = "${var.prefix}${var.volume_name}"
-  volume_count         = var.volume_count
-  volume_size = var.volume_size
+  volume_name  = "${var.prefix}${var.volume_name}"
+  volume_count = var.volume_count
+  volume_size  = var.volume_size
+  volume_path  = var.volume_path
+  instance_id  = module.app_server[*].instance_id[0]
+  instance_count  = var.instance_count
 }
-
-# attaching volume to instance
-# instance에 volume 붙이고 싶을 경우 주석 해제 후 사용
-#resource "openstack_compute_volume_attach_v2" "volume_attach" {
-#  count = var.volume_count
-#  volume_id = module.volume.volume_id
-#  instance_id = module.app_server.instance.id
-#  device = var.volume_path
-#}
